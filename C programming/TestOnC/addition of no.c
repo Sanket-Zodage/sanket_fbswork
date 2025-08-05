26d2faf2c76ddeb6c5abb65e6d9f4ae5fed21e7a
Sanket-Zodage <zodagesanket820@gmail.com>
2025-08-05 16:53:54 +0530

    Add files via upload

---
+++ C programming/TestOnC/addition of no.c
@@ -0,0 +1,6 @@
+void main()
+{
+	int a =20,b=27,c;
+	c=a+b;
+	printf("addition of two number%d",c);
+}
---
+++ C programming/TestOnC/perecentage of sub.c
@@ -0,0 +1,8 @@
+void main ()
+{
+    float sub1=73,sub2=54,sub3=87,sub4=43,sub5=98,sum,percentage;
+	sum =sub1+sub2+sub3+sub4+sub5;
+	printf("the sum is %f",sum);
+	percentage=(sum/500)*100;
+	printf("percentage is %f",percentage);
+}
