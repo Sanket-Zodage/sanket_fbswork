ee07054638d67ecbe456f0cc39a6f2daf3a14293
Sanket-Zodage <zodagesanket820@gmail.com>
2025-08-06 10:32:28 +0530

    Delete C programming/TestOnC directory

--- C programming/TestOnC/hours and minute.c
+++
@@ -1,7 +0,0 @@
-void main()
-{
-	int min=122,hours,remining;
-	hours=min/60;
-	remining=min%60;
-	printf("hour is %d,minute is %d",hours,remining);
-}
--- C programming/TestOnC/parameter of rectangle.c
+++
@@ -1,6 +0,0 @@
-void main()
-{
-	int length=12,width=16,parameter;
-	parameter = (length+width)*2;
-	printf("parameter is %d",parameter);
-}
